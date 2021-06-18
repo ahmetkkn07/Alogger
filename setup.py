bb75e6b491edcc4c5a0c2bd048477dd265bc2cf1
Ahmet KÖKEN <ahmetkkn07@gmail.com>
2021-06-19 01:20:27 +0300

    added pypi staffs

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='Alogger',
+    packages=['Alogger'],  # this must be the same as the name above
+    version='0.1',
+    description='A python package for colored logging.',
+    author='Ahmet KÖKE',
+    author_email='ahmetkkn07@gmail.com',
+    url='https://github.com/ahmetkkn07/Alogger',  # Github URL
+    download_url='https://github.com/ahmetkkn07/Alogger/tarball/0.1',
+    # Tanımlayıcı anahtar kelimeler
+    keywords=['logger', 'logging', 'python', "colored"],
+    classifiers=[],
+)
