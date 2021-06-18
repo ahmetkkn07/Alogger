32f96dab14fed617629f2f8234bf3cffa288b0a7
Ahmet KÖKEN <ahmetkkn07@gmail.com>
2021-06-19 01:21:32 +0300

    PyPI v0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='Alogger',
     packages=['Alogger'],  # this must be the same as the name above
-    version='0.1',
+    version='0.2',
     description='A python package for colored logging.',
     author='Ahmet KÖKE',
     author_email='ahmetkkn07@gmail.com',
     url='https://github.com/ahmetkkn07/Alogger',  # Github URL
-    download_url='https://github.com/ahmetkkn07/Alogger/tarball/0.1',
+    download_url='https://github.com/ahmetkkn07/Alogger/tarball/0.2',
     # Tanımlayıcı anahtar kelimeler
     keywords=['logger', 'logging', 'python', "colored"],
     classifiers=[],
